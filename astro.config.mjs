042f54464da5c0e0975ee9740790a7cbf5aba1ef
juanquiro <juanquirozsanchez@hotmail.com>
2025-07-20 12:18:32 -0400

    astro cambios para build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 
 export default defineConfig({
   site: 'https://tu-dominio.com',
@@ -11,8 +10,5 @@ export default defineConfig({
     mdx(), 
     sitemap(), 
     tailwind({}),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    })
   ],
 });
