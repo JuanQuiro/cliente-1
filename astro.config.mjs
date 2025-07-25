bb1315bad97b5b14f9d83b3a813ee3728f670b8c
Juan Quiro <juanquirozsanchez@hotmail.com>
2025-07-24 20:23:45 -0400

    server version

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://tu-dominio.com',
+  output: 'server',
   adapter: vercel({}),
   integrations: [
     mdx(), 
